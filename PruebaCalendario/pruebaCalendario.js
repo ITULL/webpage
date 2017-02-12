9d68caf4650208d92f3f5f5527094154476238f2
Rafael <alu0100891812@ull.edu.es>
2017-02-12 15:52:30 +0000

    Calendario con dia resaltado

--- PruebaCalendario/pruebaCalendario.js
+++ PruebaCalendario/pruebaCalendario.js
@@ -11,7 +11,7 @@ var mesesNombre = ["January", "February", "March", "April", "May", "June", "July
 document.getElementById('calendar').innerHTML = '<div id="calendarEncabezado"><button id="calenarToday" onclick="IrHoy()">Hoy</button><button id="calendarMonthPrev" onclick="uptMonth(-1)">Prev</button><span id="calendarMonthName"></span><button id="calendarMonthNext" onclick="uptMonth(1)">Next</button></div><div id="semanas"><div id="titsem"><div class="day">Lunes</div><div class="day">Martes</div><div class="day">Miercoles</div><div class="day">Jueves</div><div class="day">Viernes</div><div class="day">Sabado</div><div class="day">Domingo</div></div><div id="sem0" class="sem"><div class="day" id="d01"></div><div class="day" id="d02"></div><div class="day" id="d03"></div><div class="day" id="d04"></div><div class="day" id="d05"></div><div class="day" id="d06"></div><div class="day" id="d07"></div></div><div id="sem1" class="sem"><div class="day" id="d08"></div><div class="day" id="d09"></div><div class="day" id="d10"></div><div class="day" dia="d11"></div><div class="day" id="d12"></div><div class="day" id="d13"></div><div class="day" id="d14"></div></div><div id="sem2" class="sem"><div class="day" id="d15"></div><div class="day" id="d16"></div><div class="day" id="d17"></div><div class="day" id="d18"></div><div class="day" id="d19"></div><div class="day" id="d20"></div><div class="day" id="d21"></div></div><div id="sem3" class="sem"><div class="day" id="d22"></div><div class="day" id="d23"></div><div class="day" id="d24"></div><div class="day" id="d25"></div><div class="day" id="d26"></div><div class="day" id="d27"></div><div class="day" id="d28"></div></div><div id="sem4" class="sem"><div class="day" id="d29"></div><div class="day" id="d30"></div><div class="day" id="d31"></div><div class="day" id="d32"></div><div class="day" id="d33"></div><div class="day" id="d34"></div><div class="day" id="d35"></div></div><div id="sem5" class="sem"><div class="day" id="d36"></div><div class="day" id="d37"></div><div class="day" id="d38"></div><div class="day" id="d39"></div><div class="day" id="d40"></div><div class="day" id="d41"></div><div class="day" id="d42"></div></div></div>';
 
 //Rellenar  datos calendario
-dibujarCalendario();
+dibujarCalendario(1);
 
 //Get pos de vector circular
 function getEleVec(vec,pos) {
@@ -22,10 +22,9 @@ function getEleVec(vec,pos) {
     }
 }
 
-function dibujarCalendario() {
+function dibujarCalendario(dibujaHoy) {
     //Calculo variables imporatantes de la fecha
     var fecha = date.toDateString();
-    var mes = fecha.slice(4,7);
     var diaMes = fecha.slice(8,10);
     var diaSemanaNum = date.getDay()-1;
     var mesNum = date.getMonth();
@@ -35,7 +34,6 @@ function dibujarCalendario() {
     if(comienzo<0){
         comienzo = 7 + comienzo;
     }
-
     //Relleno encabezado
     document.getElementById('calendarMonthName').innerHTML = getEleVec(mesesNombre, mesNum);
 
@@ -84,6 +82,76 @@ function dibujarCalendario() {
             }
         }
     }
+
+    //Resalta el dia de hoy
+    if(dibujaHoy){
+        //Selecciona el dia de hoy
+        var Hoy = document.getElementsByClassName('day');
+        Hoy = Hoy[(6 + parseInt(diaMes) + comienzo)];
+        Hoy.style.backgroundColor = '#ffffff';
+
+        var temp, g, b, idb;
+        //Inicia el resaltado
+        var ids = setInterval(subirColor, 20);
+
+        //Cambia entre intervalos
+        function changeInterval(){
+            clearInterval(ids);
+            idb = setInterval(bajarColor, 20);
+        }
+
+        //Pinta el color
+        function subirColor() {
+            //Coge el color actual
+            temp = Hoy.style.backgroundColor;
+            temp = temp.replace('rgb(','').replace(')','').replace(' ','').split(',');
+            //Guarda el canal verde y azul
+            g = parseInt(temp[1]);
+            b = parseInt(temp[2]);
+            if(g==15&&b==15) {
+                changeInterval();
+            }else{
+                g -= 16;
+                b -= 16;
+                //Se convierte a hexadecimal
+                b = b.toString(16);
+                if(b.length==1){
+                    b = '0' + b.toString(16);
+                }
+                g = g.toString(16);
+                if(g.length==1){
+                    g = '0' + g.toString(16);
+                }
+
+                Hoy.style.backgroundColor = '#ff' + g + b;
+            }
+        }
+
+        //Borra el color
+        function bajarColor() {
+            temp = Hoy.style.backgroundColor;
+            temp = temp.replace('rgb(','').replace(')','').replace(' ','').split(',');
+            g = parseInt(temp[1]);
+            b = parseInt(temp[2]);
+            if(g==255&&b==255) {
+                clearInterval(idb);
+            }else{
+                g += 16;
+                b += 16;
+
+                b = b.toString(16);
+                if(b.length==1){
+                    b = '0' + b.toString(16);
+                }
+                g = g.toString(16);
+                if(g.length==1){
+                    g = '0' + g.toString(16);
+                }
+
+                Hoy.style.backgroundColor = '#ff' + g + b;
+            }
+        }
+    }
 }
 
 //Aumentar o disminuir el mes
@@ -107,11 +175,11 @@ function uptMonth(uptVal) {
     date = new Date(fechaNueva);
 
     //Dibuja el calendario
-    dibujarCalendario();
+    dibujarCalendario(0);
 }
 
 function IrHoy() {
     date = new Date();
 
-    dibujarCalendario();
+    dibujarCalendario(1);
 }
